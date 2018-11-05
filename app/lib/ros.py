bb2d8d218053f6ee80ffc95adc643ffe3e949193
leontius <albireo@live.cn>
2018-11-05 08:43:08 +0000

    fix(api): ros_api msg subscriber.

--- app/lib/ros.py
+++ app/lib/ros.py
@@ -1,37 +1,38 @@
 # -*- coding: utf-8 -*-
 import time
-
-import rosbag
 import roslaunch
 import rospy
 from roslaunch.parent import ROSLaunchParent
 from sensor_msgs.msg import PointCloud2
+from pypcd import PointCloud
 
 
 class RosCommon:
 
-    def __init__(self, bag='.bag', init_node=False):
+    def __init__(self, init_node=False):
         self.init_node = init_node
-        self.bag = rosbag.Bag('/data/' + time.strftime("%Y%m%d%H%M%S", time.localtime()) + bag, 'w')
-
         if not self.init_node:
             rospy.init_node('listener', disable_signals=True, anonymous=True)
+            self.pub = rospy.Publisher('out_cloud', PointCloud2, queue_size=1)
 
     def __del__(self):
         if self.init_node:
             rospy.signal_shutdown('ROS_Wait_For_Msg done')
 
-    def process_pcd_data(self, data):
-        self.bag.write('PointCloud2', data)
+    def process_pcd_data(self, msg):
+        pc = PointCloud.from_msg(msg)
+        pcd_file = '/data/' + time.strftime("%Y%m%d%H%M", time.localtime()) + '.pcd'
+        if len(pc.pc_data) > 1000:
+            pc.save(pcd_file, compression='ascii')
+        # outmsg = pc.to_msg()
+        # self.pub.publish(outmsg)
 
     def record(self, topic='/hokuyo_points'):
         rospy.Subscriber(topic, PointCloud2, self.process_pcd_data)
-
+        # self.pub = rospy.Publisher('out_cloud', PointCloud2, self.process_pcd_data)
         rate = rospy.Rate(1)  # 10hz
         while not rospy.is_shutdown():
             rate.sleep()
-        else:
-            self.bag.close()
 
     @staticmethod
     def close():
